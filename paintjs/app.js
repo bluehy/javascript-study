ab5e13a031fae34cfbf0ec48109c20842b561556
hy <chessg55@gmail.com>
2021-03-22 20:03:02 +0900

    getComputedStyle, getPropertyValue

--- paintjs/app.js
+++ paintjs/app.js
@@ -2,6 +2,7 @@
 
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("colors__color");
 
 // default canvas width,height
 canvas.width = 1400;
@@ -28,22 +29,35 @@ function onMouseMove(event){
    const x = event.offsetX * 2;
    const y = event.offsetY * 2;
    if(!painting){
+      // creating path
       ctx.beginPath();
       ctx.moveTo(x, y);
    } else {
+      // stroke - visible line
       ctx.lineTo(x, y);
       ctx.stroke();
    }
 }
 
 // canvas 클릭시 이벤트
-function onMouseDown(event){
-   painting = true;
+// function onMouseDown(event){
+//    painting = true;
+// }
+
+// change color
+function handleColorClick(event){
+   const picked = window.getComputedStyle(event.target);
+   const color = picked.getPropertyValue('background-color');
+   ctx.strokeStyle = color;
+   // console.log(picked.getPropertyValue('background-color'));
 }
 
+// 이벤트 발생 - 기능
 if(canvas){
    canvas.addEventListener("mousemove", onMouseMove)
    canvas.addEventListener("mousedown", startPainting);
    canvas.addEventListener("mouseup", stopPainting);
    canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(item => item.addEventListener("click", handleColorClick));
