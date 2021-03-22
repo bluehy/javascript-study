92906690433205074855f8fb8eb609421b1498f1
hy <chessg55@gmail.com>
2021-03-22 17:48:43 +0900

    default canvas, ctx set up, basic painting function

--- paintjs/app.js
+++ paintjs/app.js
@@ -1,34 +1,49 @@
 // app.js
 
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+// default canvas width,height
+canvas.width = 1400;
+canvas.height = 1000;
+// default ctx
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = "2.5";
 
 
-//painting 기능
+//painting 상태 on/off
 let painting = false;
 
 function stopPainting(event){
    painting = false;
 }
 
-// canvas 위에서의 brush location 파악
+// 그리기 기능
+function startPainting(x, y){
+   painting = true;
+}
+
+// canvas 위에서의 brush location 파악 + drawing path
 function onMouseMove(event){
-   const x = event.offsetX;
-   const y = event.offsetY;
+   const x = event.offsetX * 2;
+   const y = event.offsetY * 2;
+   if(!painting){
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+   } else {
+      ctx.lineTo(x, y);
+      ctx.stroke();
+   }
 }
 
-// canvas 클릭
+// canvas 클릭시 이벤트
 function onMouseDown(event){
-   painting = true;   
-}
-
-// canvas 클릭 X
-function onMouseUp(event){
-   stopPainting();
+   painting = true;
 }
 
 if(canvas){
    canvas.addEventListener("mousemove", onMouseMove)
-   canvas.addEventListener("mousedown", onMouseDown);
-   canvas.addEventListener("mouseup", onMouseUp);
+   canvas.addEventListener("mousedown", startPainting);
+   canvas.addEventListener("mouseup", stopPainting);
    canvas.addEventListener("mouseleave", stopPainting);
 }
