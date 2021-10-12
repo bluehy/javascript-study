d478403319a8fd897835e4b97c6fc551f8947994
hayoung <chessg55@gmail.com>
2021-10-12 16:27:05 +0900

    input value

--- momentum2021/app.js
+++ momentum2021/app.js
@@ -1,1 +1,16 @@
-alert("hi");
+const loginForm = document.querySelector("#loginForm");
+// querySelector로는 id, class 모두 찾아낼 수 있기 때문에 id,class구분을 해줘야한다.
+// = const loginForm = document.getElementById("loginForm");
+   const loginInput = loginForm.querySelector("input");
+   // const loinInput = document.querySelector("#loginForm input"); 도 가능.
+   const loginButton = loginForm.querySelector("button");
+
+
+const handleBtnClick = () => {
+   console.dir(loginInput);
+   // input의 내용이 object형식으로 출력
+   console.log(loginInput.value);
+   console.log("click!");
+};
+
+loginButton.addEventListener("click",handleBtnClick)
