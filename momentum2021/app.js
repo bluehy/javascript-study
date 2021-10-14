9143b9e7ac6354f16866c5ee587235ee19ad19b2
hayoung <chessg55@gmail.com>
2021-10-15 01:31:53 +0900

    make loginForm-Getting username step1

--- momentum2021/app.js
+++ momentum2021/app.js
@@ -7,32 +7,10 @@ const loginForm = document.querySelector("#loginForm");
    const link = document.querySelector("a");
 
 const onLoginSubmit = (e) => {
-   // console.dir(loginInput);
-   // // input의 내용이 object형식으로 출력
-   // console.log(loginInput.value);
-   // console.log("click!");
    e.preventDefault();
-   // 기본 동작을 prevent하는 방법.
-
    const username = loginInput.value;
-   if(username === "") {
-      alert("이름을 입력해주세요.");
-   }else if(username.length > 15) {
-      alert(`이름이 너무 깁니다. 15자 미만으로 설정해주세요. (현재 글자수 : ${username.length}자)`)
-   }
-   // 유효성 검사 :: only JS 
+   loginForm.classList.add("hidden");
+   console.log(username);
 };
 
-const handleLinkClick = (e) => {
-   e.preventDefault();
-   // -> {defaultPrevented: true}
-   alert("clicked!"); /* 일시적으로 모든 동작을 정지시킴 */
-}
-
-// loginButton.addEventListener("click",handleBtnClick);
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
-// event가 발생할 때마다 지정된 함수를 수행한다.
-// 함수는 실행될 때, event에 대한 정보를 담은 object를 함께 전달한다. 
-// function({information about the event that just happened})
-// (이 때문에 기본 기능이 작동됨. ex. 새로고침, 화면이동 등)
