543fac73c9945c7b50a9cf1d6a184869b43ced5d
hayoung <chessg55@gmail.com>
2021-10-14 17:29:10 +0900

    event and object

--- momentum2021/app.js
+++ momentum2021/app.js
@@ -4,14 +4,14 @@ const loginForm = document.querySelector("#loginForm");
    const loginInput = loginForm.querySelector("input");
    // const loinInput = document.querySelector("#loginForm input"); 도 가능.
    // const loginButton = loginForm.querySelector("button");
+   const link = document.querySelector("a");
 
-
-const onLoginSubmit = () => {
+const onLoginSubmit = (e) => {
    // console.dir(loginInput);
    // // input의 내용이 object형식으로 출력
    // console.log(loginInput.value);
    // console.log("click!");
-   event.preventDefault();
+   e.preventDefault();
    // 기본 동작을 prevent하는 방법.
 
    const username = loginInput.value;
@@ -23,5 +23,13 @@ const onLoginSubmit = () => {
    // 유효성 검사 :: only JS 
 };
 
+const handleLinkClick = (e) => {
+   alert("clicked!"); /* 일시적으로 모든 동작을 정지시킴 */
+   e.preventDefault();
+}
+
 // loginButton.addEventListener("click",handleBtnClick);
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+// event가 발생할 때마다 지정된 함수를 수행한다.
+// 함수는 실행될 때, event에 대한 정보를 담은 object를 함께 전달한다. (이 때문에 기본 기능이 작동됨. ex. 새로고침, 화면이동 등)
