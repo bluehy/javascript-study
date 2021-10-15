8e67830aadd85ddaad92e3c1210b23ae6c3af9a0
hayoung <chessg55@gmail.com>
2021-10-16 01:27:55 +0900

    Saving, Loading Username

--- momentum2021/app.js
+++ momentum2021/app.js
@@ -3,20 +3,32 @@ const loginForm = document.querySelector("#loginForm");
    const greeting = document.querySelector("#greetingUser");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 // -----------------------------------------------------------
 
 const onLoginSubmit = (e) => {
    e.preventDefault();
+   
    loginForm.classList.add(HIDDEN_CLASSNAME);
    const username = loginInput.value;
    
    // save & remember Value localStorage
    // key & Value
    // https://developer.mozilla.org/ko/docs/Web/API/Window/localStorage
-   localStorage.setItem("username", username);
+   localStorage.setItem(USERNAME_KEY, username);
    
    greeting.innerText = `Hello ${username}`;
    greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const checkedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(checkedUsername === null){
+   // show the form
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+   // show the greeting
+   greeting.innerText = `Hello ${checkedUsername}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
