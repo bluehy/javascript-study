1f9a73347b6b1d366b0bfb688c3e60d224f34cb2
hayoung <chessg55@gmail.com>
2021-10-16 01:55:22 +0900

    new Date()

--- momentum2021/js/clock.js
+++ momentum2021/js/clock.js
@@ -11,3 +11,14 @@ const tickingTime = () => {
 // setTimeout(tickingTime, 5000);
 // 5초 후 한번 실행
 
+const date = new Date();
+date.getDay();
+// 요일
+date.getFullYear();
+// 년도
+date.getHours();
+// 시
+date.getMinutes();
+// 분
+date.getSeconds();
+// 초
