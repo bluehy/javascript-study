55a2728f84ce5759de7e689209eb2f413c4fef82
hayoung <chessg55@gmail.com>
2021-10-17 17:10:50 +0900

    time ticking

--- momentum2021/js/clock.js
+++ momentum2021/js/clock.js
@@ -2,23 +2,25 @@ const clock = document.querySelector("h2#time");
 // ---------------------------------------------
 
 const tickingTime = () => {
-   clock.innerText = "clock is here";
-   console.log("hello");
+   const date = new Date();
+   clock.innerText = `${date.getHours() ? `0${date.getHours()}`: `${date.getHours()}`}:${date.getMinutes() < 10 ? `0${date.getMinutes()}`:`${date.getMinutes()}`}:${date.getSeconds() < 10 ? `0${date.getSeconds()}`: `${date.getSeconds()}`}`;
 }
 
+tickingTime();
+setInterval(tickingTime,500);
 // setInterval(tickingTime, 5000);
 // 5초마다 실행
 // setTimeout(tickingTime, 5000);
 // 5초 후 한번 실행
 
-const date = new Date();
-date.getDay();
-// 요일
-date.getFullYear();
-// 년도
-date.getHours();
-// 시
-date.getMinutes();
-// 분
-date.getSeconds();
+// const date = new Date();
+// date.getDay();
+// // 요일
+// date.getFullYear();
+// // 년도
+// date.getHours();
+// // 시
+// date.getMinutes();
+// // 분
+// date.getSeconds();
 // 초
