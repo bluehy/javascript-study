ca425101b4a156fecc5f0960834b7bf4f8949193
hayoung <chessg55@gmail.com>
2021-10-18 17:52:57 +0900

    append randomImage

--- momentum2021/js/background.js
+++ momentum2021/js/background.js
@@ -5,5 +5,10 @@ const images = [
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
 // =========================================================
 
+bgImage.src = `./img/${chosenImage}`;
+// create html element in javascript
+document.body.appendChild(bgImage);
