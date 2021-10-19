49ac9add22858245a5421dad33cd345ad1e70ddf
hayoung <chessg55@gmail.com>
2021-10-19 10:10:12 +0900

    saved value

--- momentum2021/js/todo.js
+++ momentum2021/js/todo.js
@@ -1,10 +1,17 @@
-const toDoForm = document.querySelector("#todoList");
-const toDoUl = toDoForm.querySelector("#todoUl");
+const toDoForm = document.querySelector("#todoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoUl = document.querySelector("#todoUl");
+
 
 // ==================================================
 
 const handleToDoSubmit = (event) => {
    event.preventDefault();
+   const newTodo = toDoInput.value;
+   // newTodo에 담은 후, toDoInput의 value를 비우는 것.
+   toDoInput.value = "";
+   
+   // 입력한 todo 화면에 저장하기
 };
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
