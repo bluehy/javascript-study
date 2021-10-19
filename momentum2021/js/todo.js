fbaf17776085606d7ab86b86c09956b13a9daa89
hayoung <chessg55@gmail.com>
2021-10-19 14:40:40 +0900

    Loading todoList, re-saved Todos in LocalStorage

--- momentum2021/js/todo.js
+++ momentum2021/js/todo.js
@@ -2,14 +2,17 @@ const toDoForm = document.querySelector("#todoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoUl = document.querySelector("#todoUl");
 
-const savedTodos = [];
+const TODOS_KEY = "todoList";
+
+const toDos = [];
 // ==================================================
 
 const savedTodo = () => {
-   // localStorage.setItem("todoList", savedTodos);
+   // localStorage.setItem(TODOS_KEY, toDos);
    // array그대로 string형식으로 LocalStorage에 저장하기 위해 JSON.stringify를 사용. 
    // (LS에는 string형태로밖에 저장이 안됨)
-   localStorage.setItem("todoList", JSON.stringify(savedTodos));
+   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+   // string을 JSON.parse를 통해 js object, array로 전환
 }
 
 const deleteTodo = (e) => {
@@ -47,13 +50,29 @@ const handleToDoSubmit = (event) => {
    // newTodo에 담은 후, toDoInput의 value를 비우는 것.
    toDoInput.value = "";
    
-   // push todos into savedTodos(localStorage)
-   savedTodos.push(newTodo);
-   // console.log(savedTodos);
+   // push todos into toDos(localStorage)
+   toDos.push(newTodo);
+   // console.log(toDos);
    savedTodo();
 
    // 입력한 todo 화면에 저장하기
    paintToDo(newTodo);
 };
 
+const LoadingList = (item) => {
+   toDos.push(item);
+   savedTodo();
+   paintToDo(item);
+}
+
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if(savedTodos !== null) {
+   const parsedTodos = JSON.parse(savedTodos);
+   // console.log(parsedTodos);
+   // array의 각 item에 function실행
+   // array.forEach(function);
+   parsedTodos.forEach(LoadingList);
+
+};
