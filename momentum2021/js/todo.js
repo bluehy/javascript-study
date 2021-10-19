a66b2459cb88e6fcea15ce1c527fcf9217460fdf
hayoung <chessg55@gmail.com>
2021-10-19 10:32:29 +0900

    adding Todo

--- momentum2021/js/todo.js
+++ momentum2021/js/todo.js
@@ -5,6 +5,16 @@ const toDoUl = document.querySelector("#todoUl");
 
 // ==================================================
 
+const paintToDo = (savedTodo) => {
+   // console.log(savedTodo);
+   const myLi = document.createElement("li");
+   const mySpan = document.createElement("span");
+   myLi.appendChild(mySpan);
+   mySpan.innerText = `${savedTodo}`;
+   toDoUl.appendChild(myLi);
+}
+
+
 const handleToDoSubmit = (event) => {
    event.preventDefault();
    const newTodo = toDoInput.value;
@@ -12,6 +22,7 @@ const handleToDoSubmit = (event) => {
    toDoInput.value = "";
    
    // 입력한 todo 화면에 저장하기
+   paintToDo(newTodo);
 };
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
