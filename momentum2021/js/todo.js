cb72f57be4dc67344ead9ddae02392d70545f3ff
hayoung <chessg55@gmail.com>
2021-10-19 10:52:09 +0900

    add function of deleting Todo

--- momentum2021/js/todo.js
+++ momentum2021/js/todo.js
@@ -5,12 +5,31 @@ const toDoUl = document.querySelector("#todoUl");
 
 // ==================================================
 
+const deleteTodo = (e) => {
+   // console.log(e);
+   // console.dir(e.target);
+   const targetLi = e.target.parentElement;
+   // target은 event의 html element 정보를 줌
+   // console.log(targetLi);
+   targetLi.remove();
+}
+
 const paintToDo = (savedTodo) => {
    // console.log(savedTodo);
    const myLi = document.createElement("li");
    const mySpan = document.createElement("span");
+   const closeBtn = document.createElement("button");
+   
+   // add todo list
    myLi.appendChild(mySpan);
    mySpan.innerText = `${savedTodo}`;
+
+   // add delete button
+   closeBtn.innerText = "X";
+   myLi.appendChild(closeBtn);
+   closeBtn.addEventListener("click", deleteTodo);
+
+   // paint TodoUl
    toDoUl.appendChild(myLi);
 }
 
