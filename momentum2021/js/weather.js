18bae27cfe4983a3f7bc6aadf9ed0a7657669948
hayoung <chessg55@gmail.com>
2021-10-20 16:31:27 +0900

    get latitude, longitude

--- momentum2021/js/weather.js
+++ momentum2021/js/weather.js
@@ -1,11 +1,13 @@
 const onGeoFound = (position) => {
-   console.log(position);
+   const lat = position.coords.latitude;
+   const lng = position.coords.longitude;
+   console.log(`You live in`, lat, lng);
 };
 
 const onGeoError = () => {
    alert("위치 정보를 찾지 못했습니다. 날씨 정보를 제공할 수 없습니다.")
 }
 
-
+// geolocationposition MDN : https://developer.mozilla.org/ko/docs/Web/API/GeolocationPosition
 // navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
 navigator.geolocation.getCurrentPosition(onGeoFound,onGeoError)
